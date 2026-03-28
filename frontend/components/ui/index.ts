0878281849ea2ed1062204ef2dbdaecb12f86075
luckarts <luc.bachelerieart@gmail.com>
2026-03-28 20:14:48 +0100

    feat(ui): add AppLink atom and ui/index.ts barrel, use AppLink in auth layout

---
+++ frontend/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { default as AppLogo } from './AppLogo.vue'
+export { default as Button } from './Button.vue'
+export { default as Input } from './Input.vue'
+export { default as Toast } from './Toast.vue'
+export { default as ToastContainer } from './ToastContainer.vue'
+export { default as AppLink } from './AppLink.vue'
+
